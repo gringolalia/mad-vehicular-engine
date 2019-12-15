2706b3739d7f9a335eed33d8e11968013f3f56ff
Colin Brayton <gringolalia@gmail.com>
2019-12-15 17:44:31 -0300

    dummy page initialed

--- src/App.vue
+++ src/App.vue
@@ -9,27 +9,18 @@
 </template>
 
 <style lang="scss">
+
+@import url('https://fonts.googleapis.com/css?family=News+Cycle&display=swap');
+
 $image-path: '~@/../mdb/mdbvue/img';
 @import '~@/../mdb/mdbvue/scss/mdb-free.scss';
 
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+#app, html {
+  font-family: 'News Cycle', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
