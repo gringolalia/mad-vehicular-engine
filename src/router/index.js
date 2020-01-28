2716ad687779dc4f5c68bd2a580059f9b871ae2b
Colin Brayton <gringolalia@gmail.com>
2020-01-28 12:41:04 -0300

    now on netlify?

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+// import Home from '../views/Home.vue'
+// import Home from '../views/Home.vue'
+// import Home from '../views/Home.vue'
+// import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
