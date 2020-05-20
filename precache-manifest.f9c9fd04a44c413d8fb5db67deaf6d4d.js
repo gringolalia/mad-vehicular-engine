59702ef838cf22bc53ca98dd01d693e0a6e941d6
Colin Brayton <gringolalia@gmail.com>
2020-05-20 13:38:48 -0300

    deploy

---
+++ precache-manifest.f9c9fd04a44c413d8fb5db67deaf6d4d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a38751cf59b8f9e4a7e0",
+    "url": "/mad-vehicular-engine/css/about.180a9cb1.css"
+  },
+  {
+    "revision": "af1910af6e7c35bd8a15",
+    "url": "/mad-vehicular-engine/css/app.46626b43.css"
+  },
+  {
+    "revision": "25f1a80b36f8829940c3",
+    "url": "/mad-vehicular-engine/css/chunk-vendors.da170bcc.css"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/mad-vehicular-engine/img/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/mad-vehicular-engine/img/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "9b6200320cf7c0cc32f17498808d2108",
+    "url": "/mad-vehicular-engine/img/ponteavmarinho.9b620032.jpg"
+  },
+  {
+    "revision": "bf8afdf2265f192596f96400949d6943",
+    "url": "/mad-vehicular-engine/img/titannic.bf8afdf2.jpg"
+  },
+  {
+    "revision": "09c8183f2e99f3861e86a1113b17d48b",
+    "url": "/mad-vehicular-engine/index.html"
+  },
+  {
+    "revision": "a38751cf59b8f9e4a7e0",
+    "url": "/mad-vehicular-engine/js/about.5da42b83.js"
+  },
+  {
+    "revision": "af1910af6e7c35bd8a15",
+    "url": "/mad-vehicular-engine/js/app.9f64d79c.js"
+  },
+  {
+    "revision": "25f1a80b36f8829940c3",
+    "url": "/mad-vehicular-engine/js/chunk-vendors.768ba868.js"
+  },
+  {
+    "revision": "8a0474dce8a8eea3ac46a75d3a68242c",
+    "url": "/mad-vehicular-engine/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/mad-vehicular-engine/robots.txt"
+  }
+]);
